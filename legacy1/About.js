9638f9f9f331d49a5423cbf22ff0563ddd36f076
Johan Nordberg <johan@ifokus.se>
2023-07-22 13:01:16 +0200

    Moved src to root

--- legacy1/About.js
+++
@@ -1,9 +0,0 @@
-import GDrive from "./GDrive";
-
-export default class About {
-    get(queryParams) {
-        return fetch(`${GDrive._urlAbout}${GDrive._stringifyQueryParams(queryParams)}`, {
-            headers: GDrive._createHeaders()
-        });
-    }
-}
